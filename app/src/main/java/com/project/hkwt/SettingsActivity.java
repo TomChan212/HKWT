ce5a8b7bfed703c9f8365f99984bb0c5500ac930
TomChan212 <a1418286995@gmail.com>
2023-04-13 22:41:08 +0800

    Initial commit

---
+++ app/src/main/java/com/project/hkwt/SettingsActivity.java
@@ -0,0 +1,76 @@
+package com.project.hkwt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    public TextView text_language_selection;
+    public Button confirm_button;
+    public Button cancel_button;
+
+    RadioGroup langSelections;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        text_language_selection = findViewById(R.id.txt_setting_lang);
+        confirm_button = findViewById(R.id.saveSettings);
+        cancel_button = findViewById(R.id.noSave);
+        String lang = Settings.getLang();
+        switch (lang){
+            case "en":
+                text_language_selection.setText("Switch Language:");
+                confirm_button.setText("CONFIRM");
+                cancel_button.setText("CANCEL");
+                break;
+            case "sc":
+                text_language_selection.setText("切换语言:");
+                confirm_button.setText("确认");
+                cancel_button.setText("取消");
+                break;
+            case "tc":
+                text_language_selection.setText("轉換語言:");
+                confirm_button.setText("確認");
+                cancel_button.setText("取消");
+                break;
+        }
+        langSelections = findViewById(R.id.lang_group);
+        langSelections.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.lang_en:
+                        Settings.setSelection("en");
+                        break;
+                    case R.id.lang_sc:
+                        Settings.setSelection("sc");
+                        break;
+                    case R.id.lang_tc:
+                        Settings.setSelection("tc");
+                        break;
+                }
+            }
+        });
+        confirm_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Settings.setLang(Settings.getSelection());
+                Intent intent = new Intent(SettingsActivity.this, InnerActivity.class);
+                startActivity(intent);
+            }
+        });
+        cancel_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
+}
