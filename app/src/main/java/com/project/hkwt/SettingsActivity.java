6ff84fa2bc6a2d545da79eb47cec782148d03f00
TomChan212 <a1418286995@gmail.com>
2023-04-14 01:29:42 +0800

    add somethings

--- app/src/main/java/com/project/hkwt/SettingsActivity.java
+++ app/src/main/java/com/project/hkwt/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.project.hkwt;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +10,8 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
     public TextView text_language_selection;
@@ -48,12 +52,15 @@ public class SettingsActivity extends AppCompatActivity {
                 switch (checkedId){
                     case R.id.lang_en:
                         Settings.setSelection("en");
+                        setLocale("en");
                         break;
                     case R.id.lang_sc:
                         Settings.setSelection("sc");
+                        setLocale("ab");
                         break;
                     case R.id.lang_tc:
                         Settings.setSelection("tc");
+                        setLocale("aa");
                         break;
                 }
             }
@@ -72,5 +79,13 @@ public class SettingsActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    private void setLocale(String lang){
+    Locale locale = new Locale(lang);
+    Locale.setDefault(locale);
+    Resources res = getResources();
+    Configuration conf = res.getConfiguration();
+    conf.locale = locale;
+    res.updateConfiguration(conf, res.getDisplayMetrics());
     }
 }
