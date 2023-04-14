b3a64964d91924445b21c5eb7e1588c92c65831b
TomChan212 <101737857+TomChan212@users.noreply.github.com>
2023-04-14 23:08:28 +0800

    Update LoginActivity.java

--- app/src/main/java/com/project/hkwt/LoginActivity.java
+++ app/src/main/java/com/project/hkwt/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.project.hkwt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -107,6 +109,15 @@ public class LoginActivity extends AppCompatActivity {
                         intent.putExtra("email", emailFromDB);
                         intent.putExtra("username", usernameFromDB);
                         intent.putExtra("password", passwordFromDB);
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("name", nameFromDB);
+                        editor.putString("email", emailFromDB);
+                        editor.putString("username", usernameFromDB);
+                        editor.putString("password", passwordFromDB);
+                        editor.commit();
+                        Log.d("LoginActivity", "Username: " + usernameFromDB + ", Email" + emailFromDB+", Password: " + passwordFromDB);
 
                         startActivity(intent);
                     } else {
